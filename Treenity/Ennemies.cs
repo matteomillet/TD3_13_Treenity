d834bcf7af7ce4c338654b10580d84aa3d12ebd3
matteomillet <matteo.millet-leite-antunes@etu.univ-smb.fr>
2025-12-10 14:55:22 +0100

    Class Ennemies

---
+++ Treenity/Ennemies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treenity
+{
+    internal class Ennemies
+    {
+        String name;
+        string style = "Ressources/Images/pinguin.png";
+        int pv;
+        int strength;
+    }
+}
