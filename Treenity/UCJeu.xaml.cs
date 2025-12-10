03c275971c24ed6133cb328c96e78b6ab71c97ad
LucasDecroux <Lucas.Decroux@etu.univ-smb.fr>
2025-12-10 16:46:31 +0100

    Commit rectangle perso

--- Treenity/UCJeu.xaml.cs
+++ Treenity/UCJeu.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Drawing;
 
 
 
@@ -26,11 +26,16 @@ namespace Treenity
     public partial class UCJeu : UserControl
     {
         Ennemies ennemie1 = new Ennemies();
+        public System.Drawing.Rectangle rectangleJoueur = new System.Drawing.Rectangle();
         public UCJeu()
         {
             InitializeComponent();
             AffichageEntite(ennemie1);
-            
+
+            rectangleJoueur.X = (int) Canvas.GetLeft(imgPerso);
+            rectangleJoueur.Y = (int)Canvas.GetTop(imgPerso);
+            rectangleJoueur.Height = (int) imgPerso.Height;
+            rectangleJoueur.Width = (int)imgPerso.Width;
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
@@ -47,22 +52,24 @@ namespace Treenity
         {
             if (e.Key == Key.Right || e.Key == Key.D)
             {
-                imgPerso.RenderTransformOrigin = new Point(0.5, 0.5);
+                imgPerso.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
                 ScaleTransform fliptrans = new ScaleTransform();
                 fliptrans.ScaleX = 1;
                 imgPerso.RenderTransform = fliptrans;
                 Canvas.SetLeft(imgPerso, Canvas.GetLeft(imgPerso) + 2);
+                rectangleJoueur.X = rectangleJoueur.X + 2;
+                
             }
 
 
             if (e.Key == Key.Left || e.Key == Key.Q)
             {
-                imgPerso.RenderTransformOrigin = new Point(0.5, 0.5);
+                imgPerso.RenderTransformOrigin = new System.Windows.Point(0.5, 0.5);
                 ScaleTransform fliptrans = new ScaleTransform();
                 fliptrans.ScaleX = -1;
                 imgPerso.RenderTransform = fliptrans;
                 Canvas.SetLeft(imgPerso, Canvas.GetLeft(imgPerso) - 2);
-
+                rectangleJoueur.X = rectangleJoueur.X - 2;
             }
 
 
@@ -71,11 +78,9 @@ namespace Treenity
         public void AffichageEntite(Ennemies entite)
         {
             Image ennemieImg = new Image();
-            ennemieImg.Width = 200;
             BitmapImage bitmapImage = new BitmapImage();
             bitmapImage.BeginInit();
             bitmapImage.UriSource = new Uri(entite.style);
-            bitmapImage.DecodePixelWidth = 200;
             bitmapImage.EndInit();
             ennemieImg.Source = bitmapImage;
             canvasJeu.Children.Add(ennemieImg);
